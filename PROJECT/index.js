0bea4bb000d9d2003067e6261638123be7f379af
SooKyoung <sksohn01@naver.com>
2022-05-31 03:18:41 +0900

    2022-05-31-pc

--- PROJECT/index.js
+++ PROJECT/index.js
@@ -1,20 +1,10 @@
-// 자신의 최애 브랜드 저장 함수
-function addBrand() {
-  if (window.event.keyCode === 13) {
-    var brand_name = document.getElementById("brand_name").value;
+function show(id) {
+  var title = document.getElementById(id).id;
+  console.log(title);
+  clickEventHandle(document.getElementById(id));
+}
 
-    if (brand_name.length < 1) {
-      document.getElementById("brand_name").focus();
-      return;
-    }
-
-    var li = document.createElement("li");
-    const addList = document.createTextNode(brand_name);
-    li.appendChild(addList);
-
-    document.getElementById("brand_list").appendChild(li);
-
-    // 리스트 만든 후, input 창 초기화
-    document.getElementById("brand_name").value = "";
-  }
+function clickEventHandle(event) {
+  var target = event.target;
+  console.log(target);
 }
---
+++ PROJECT/my_lookbook.js
@@ -0,0 +1,20 @@
+// 자신의 최애 브랜드 저장 함수
+function addBrand() {
+  if (window.event.keyCode === 13) {
+    var brand_name = document.getElementById("brand_name").value;
+
+    if (brand_name.length < 1) {
+      document.getElementById("brand_name").focus();
+      return;
+    }
+
+    var li = document.createElement("li");
+    const addList = document.createTextNode(brand_name);
+    li.appendChild(addList);
+
+    document.getElementById("brand_list").appendChild(li);
+
+    // 리스트 만든 후, input 창 초기화
+    document.getElementById("brand_name").value = "";
+  }
+}
